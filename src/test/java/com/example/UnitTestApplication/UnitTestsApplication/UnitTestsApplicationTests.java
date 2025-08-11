ee03f56bb5669bc22076ff4cba70cc47b803dd22
Krishnard <krishnadharmale20@gmail.com>
2025-08-11 19:33:02 +0530

    first commit

---
+++ src/test/java/com/example/UnitTestApplication/UnitTestsApplication/UnitTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UnitTestApplication.UnitTestsApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
